39aa88696a7616b0bc90e9d71f45718f516efdc5
ranjireddy <45517070+ranjireddy@users.noreply.github.com>
2020-03-05 11:47:04 -0600

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,25 @@
+package looping;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		
+		int num;
+		int fact=1;
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter the factorial number ");
+		num=sc.nextInt();
+		
+		for(int i=1; i<=num ; i++)
+        {
+            fact *= i;
+        }
+		
+		System.out.println(fact);
+
+	}
+
+}
---
+++ First10.java
@@ -0,0 +1,15 @@
+package looping;
+
+public class First10 {
+	public static void main(String[]args) {
+		
+		int i;
+		
+		for(i=1;i<=100; i++) {
+			
+			System.out.println(i);
+		}
+		
+	}
+
+}
